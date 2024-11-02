4a3833feb39128cd485436ba09f1bf4613fbc077
Eternatus9999 <eternatus9999@gmail.com>
2024-11-02 18:16:30 +0530

    six some issues in getting data from backend

--- src/app/page/quizpage/quizpage.component.ts
+++ src/app/page/quizpage/quizpage.component.ts
@@ -2,24 +2,32 @@ import { Component } from '@angular/core';
 import { HeadingComponent } from '../../components/heading/heading.component';
 import { RouterLink } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-quizpage',
   standalone: true,
-  imports: [RouterLink,HeadingComponent],
+  imports: [RouterLink,HeadingComponent,FormsModule,CommonModule],
   templateUrl: './quizpage.component.html',
   styleUrl: './quizpage.component.css'
 })
 export class QuizpageComponent {
-  public quiz:any;
+  public quiz:any ={
+    quiz_id: "",
+    player_id: "",
+    date: "",
+    grade: "",
+    mark: 0,
+    words:[]
+  };
   constructor(private http:HttpClient){
   }
   
   public getQuiz(){
     this.http.get("http://localhost:8080/quiz/create-quiz/P001").subscribe((data)=>{
       this.quiz = data;
-      console.log(this.quiz);
-      
+      console.log(this.quiz.words);
     });
   }
 }
