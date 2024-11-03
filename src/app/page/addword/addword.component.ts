e48d92f05e7e0a6dd59100ef02666998284e2723
Eternatus9999 <eternatus9999@gmail.com>
2024-11-03 11:05:45 +0530

    store data from front end to database is complete for adding words

--- src/app/page/addword/addword.component.ts
+++ src/app/page/addword/addword.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { HeadingComponent } from '../../components/heading/heading.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-addword',
   standalone: true,
-  imports: [],
+  imports: [HeadingComponent,FormsModule,CommonModule],
   templateUrl: './addword.component.html',
   styleUrl: './addword.component.css'
 })
 export class AddwordComponent {
+  public word:any ={
+    pronunciation: "",
+    name: ""
+  };
 
+  constructor(private http:HttpClient){}
+
+  public add(){
+    this.http.post("http://localhost:8080/word/add-word",this.word).subscribe(data=>{
+      alert("Word added Successfully!");
+    });
+  }
 }
